10e8175ee2616dd7ede9590f85d1c886eab24345
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-04-08 16:46:27 +0700

    gải pt bậc 2

---
+++ javascript/bac2.js
@@ -0,0 +1,20 @@
+function kq() {
+    let numA=Number(document.getElementsByClassName('inA').value);
+    let numB=Number(document.getElementsByClassName('inB').value);
+    let numC=Number(document.getElementsByClassName('inC').value);
+    let d= (numB*numB)- (4*numA*numC);
+    let X = ((-numB-Math.sqrt(d))/(2*numA));
+    let Y = ((-numB+Math.sqrt(d))/(2*numA));
+    let E = (-numB/2*numA);
+  if (numA == 0) {
+      alert('đây không phải là hàm bậc hai!');
+  } else if (d == 0) {
+      document.getElementsByClassName('inX').value=E;
+      document.getElementsByClassName('inY').value=E;
+  } else if (d > 0) {
+    document.getElementsByClassName('inX').value=X;
+    document.getElementsByClassName('inY').value=Y;
+  } else {
+      alert('phương trình vô nghiệm');
+  }
+}
