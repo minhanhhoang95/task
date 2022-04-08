3abfa3d741f0beff265e636824dddc0567c115f7
minhanhhoang95 <hoanganhaminh23071995@gmail.com>
2022-04-08 14:02:42 +0700

    cap nhap bai tap

---
+++ javascript/baitap.js
@@ -0,0 +1,6 @@
+function kq() {
+    let numA = Number(document.getElementById('number1').value);
+    let numB = Number(document.getElementById('number2').value);
+    let tong = -numB / numA;
+    document.getElementById('number').value = tong;
+ }
